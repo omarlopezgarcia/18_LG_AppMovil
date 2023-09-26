3dcad1d6897f84585b81b9091ada19ec8361f4a9
OmarLopez <omar.lopez@vallegrande.edu.pe>
2023-09-25 20:30:08 -0500

    Acción de Botón

--- flutter_application_1/lib/presentation/screen/basic_intro.dart
+++ flutter_application_1/lib/presentation/screen/basic_intro.dart
@@ -1,6 +1,5 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/presentation/screen/clickButton.dart';
 
 class BasicIntro extends StatelessWidget {
   const BasicIntro({super.key});
@@ -8,44 +7,39 @@ class BasicIntro extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0xFFFDE8CD),
-      body: Center(
-        child: Column(
+        backgroundColor: const Color.fromARGB(255, 199, 206, 229),
+        body: Center(
+            child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             const Text('INSTITUTO',
-                  style: 
-                    TextStyle( 
-                      fontSize: 20
-                      )
-                ),
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
             const Text('VALLE GRANDE',
-                  style:
-                    TextStyle(
-                      fontSize: 40,
-                      color: Color.fromRGBO(22, 53, 156, 1),
-                      fontWeight: FontWeight.bold
-                      )
-                ),
+                style: TextStyle(
+                    fontSize: 40,
+                    color: Color.fromRGBO(22, 53, 156, 1),
+                    fontWeight: FontWeight.bold)),
             const Text('Perfeccionamos tu talento',
-                  style:
-                    TextStyle(
-                      fontSize: 20,
-                      color: Color.fromRGBO(243, 114, 35, 1),
-                      fontWeight: FontWeight.bold,
-                      fontStyle: FontStyle.italic
-                      )
-                ),
+                style: TextStyle(
+                    fontSize: 20,
+                    color: Color.fromRGBO(243, 114, 35, 1),
+                    fontWeight: FontWeight.bold,
+                    fontStyle: FontStyle.italic)),
             MaterialButton(
-              onPressed: () {},
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => buttonClick()),
+                );
+              },
               color: const Color.fromRGBO(25, 118, 210, 1),
-              textColor: Colors.white, 
+              textColor: Colors.white,
               padding: const EdgeInsets.all(20),
-              shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(20))),
+              shape: const RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(20))),
               child: const Text('Empieza ahora...'),
             )
           ],
-        )
-      )); 
+        )));
   }
 }
